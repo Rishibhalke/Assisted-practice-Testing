0ef795455861600ead48844731474bf54cd549f8
Rishikesh <rishikeshbhalke200@gmail.com>
2023-09-23 09:47:25 +0530

    done

---
+++ Javaprograms/src/javaprogram/EmplictTypeCasting.java
@@ -0,0 +1,14 @@
+package javaprogram;
+
+public class EmplictTypeCasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double a= 10.34;
+		int b = (int)a;
+		System.out.println(b);
+
+	}
+
+}
+
