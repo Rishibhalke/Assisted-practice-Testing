cf829c27c8b535e98d2a1cd05991d5c9e95b860e
Rishibhalke <144915692+Rishibhalke@users.noreply.github.com>
2023-09-23 09:51:17 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,23 @@
+package Javaproject;
+
+public class Rectangle extends Shape {
+	
+	  private int length;
+	    private int width;
+
+	    public Rectangle(int length, int width) {
+	        this.length = length;
+	        this.width = width;
+	    }
+
+	    @Override
+	    public void displayArea() {
+	        System.out.println("Area of the Rectangle: " + calculateArea());
+	    }
+
+	    public double calculateArea() {
+	        return length * width;
+	    }
+
+}
+
