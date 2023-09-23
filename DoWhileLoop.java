1ade59785267bdff92e78e610ee6cd259bec1529
Rishibhalke <144915692+Rishibhalke@users.noreply.github.com>
2023-09-23 10:00:37 +0530

    Add files via upload

---
+++ DoWhileLoop.java
@@ -0,0 +1,17 @@
+package javaprogram;
+
+public class DoWhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1;
+		do {
+			System.out.println("today is wednesday");
+			i++;
+		}
+		while(i<=7);
+
+
+	}
+
+}
---
+++ WhileLoop.java
@@ -0,0 +1,18 @@
+package javaprogram;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1;
+		while(i<=10) {
+			System.out.println(i);
+		i++;
+
+	}
+	
+	System.out.println("printed value from 1 to 10");
+
+}
+	
+}
