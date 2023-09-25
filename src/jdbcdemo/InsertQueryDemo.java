186f38cbb2559253c1278c463582833d15ae61cd
rishikesh <rishikeshbhalke200@gmail.com>
2023-09-25 08:30:04 +0000

    done

---
+++ src/jdbcdemo/InsertQueryDemo.java
@@ -0,0 +1,44 @@
+package jdbcdemo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class InsertQueryDemo {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		// TODO Auto-generated method stub
+		
+		String query ="INSET INTO movies VALUES('movie1','comedy','director1','1985')";
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		String dburl = "jdbc:mysql://localhost:3306/animated_movies";
+		String username = "root";
+		String password = "root";
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		Connection con =DriverManager.getConnection(dburl,username,password);
+		
+		String title = "newmovie";
+		String genre = "comedy";
+		String director = "director1";
+		String release_year = "1999";
+		
+		
+		PreparedStatement ps = con.prepareStatement("insert into movies values(?,?,?,?)");
+		
+		ps.setString(1,title);
+		ps.setString(2,genre);
+		ps.setString(3,director);
+		ps.setString(4,release_year);
+		
+		ps.executeUpdate();
+		
+		con.close();
+
+	}
+
+}
